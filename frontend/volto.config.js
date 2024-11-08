f404432400884b479c759805736300c026b850b4
Artur Martins Barrionuevo <artur-barrionuevo@procergs.rs.gov.br>
2024-11-08 17:43:13 -0300

    fix backend github packages

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,5 +1,13 @@
-const addons = ['volto-procergs-siteplone','volto-sitedemo'];
-const theme = '';
+const addons = ['volto-procergs-siteplone',"@eeacms/volto-accordion-block",
+  "@kitconcept/volto-button-block",
+  "@kitconcept/volto-heading-block",
+  "@kitconcept/volto-introduction-block",
+  "@kitconcept/volto-highlight-block",
+  "@kitconcept/volto-separator-block",
+  "@kitconcept/volto-light-theme",
+  "@kitconcept/volto-dsgvo-banner",
+  'volto-sitedemo'];
+const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
   addons,
