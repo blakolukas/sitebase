059cabf972ac297f80b5d50c88cae6efa382b43e
samoel-silva <samoel-silva@procergs.rs.gov.br>
2025-01-17 15:51:16 -0300

    atualiza inclusao de addons

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,10 +1,11 @@
 const addons = [
-  '@plone-collective/volto-authomatic',
-  '@kitconcept/volto-light-theme',
-  '@kitconcept/volto-dsgvo-banner',
-  'volto-sitedemo',
-  'volto-demo-tema',
-  'volto-procergs-siteplone'
+  "@plone-collective/volto-authomatic",
+  "volto-procergs-siteplone",
+  "@kitconcept/volto-light-theme",
+  "@eeacms/volto-accordion-block",
+  "@kitconcept/volto-dsgvo-banner",
+  "volto-sitedemo",
+  "volto-demo-tema"
 ];
 const theme = '@kitconcept/volto-light-theme';
 
