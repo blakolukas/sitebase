3845470fef11a02645de988307a0d023f0ae1fec
Samoel Silva <samoel-silva@procergs.rs.gov.br>
2025-03-31 14:15:49 -0300

    altera dependencias e addons incluidos

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -2,8 +2,20 @@ const addons = [
   "@plone-collective/volto-authomatic",
   "volto-procergs-siteplone",
   "@kitconcept/volto-light-theme",
-  "@eeacms/volto-accordion-block",
+
+  '@eeacms/volto-accordion-block',
+  '@kitconcept/volto-button-block',
+  '@kitconcept/volto-heading-block',
+  '@kitconcept/volto-highlight-block',
+  '@kitconcept/volto-introduction-block',
+  '@kitconcept/volto-separator-block',
+  "@codesyntax/volto-listingadvanced-variation",
   "@kitconcept/volto-dsgvo-banner",
+
+  "@eeacms/volto-hero-block",
+
+  "@kitconcept/volto-carousel-block",
+  
   "volto-sitedemo",
   "volto-demo-tema"
 ];
